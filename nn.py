20063a7f73e93d51846631ebb55308ce7ee35915
Natalia Rubio <68125155+natalia-rubio@users.noreply.github.com>
2022-06-04 20:30:44 -0700

    Add files via upload

--- nn.py
+++ nn.py
@@ -3,7 +3,8 @@ from data_extraction import *
 from sklearn.neural_network import MLPRegressor
 from sklearn.neural_network import MLPClassifier
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.model_selection import learning_curve
+import matplotlib.pyplot as plt
 def get_mse(x,y):
     return np.mean(np.power((x-y),2))
 
@@ -51,11 +52,19 @@ def construct_nn_class(X_train, target_train, X_val, target_val, activation, alp
     if verbose == True: print(f"Validation accuracy: {val_accuracy}")
 
     return nn_class, pred_train, train_accuracy, pred_val, val_accuracy
+def make_train_plot(X_basic, target_bin, train_indices, val_indices):
+    hidden_layer_sizes = (3* min(X_basic.shape),)
+    nn_class = MLPClassifier(hidden_layer_sizes, activation = "relu", solver = "adam", alpha=0.001, max_iter = 2000, random_state = 0)
+    train_sizes, train_scores, valid_scores = learning_curve(nn_class, X_basic,target_bin, train_sizes=[270],cv= [(train_indices, val_indices)])
+    pdb.set_trace()
+    plt.clf()
+    epochs = np.linspace(1,train_scores.shape[0], train_scores.shape[0])
 
 def main():
-    X_basic, X_technical, target_bin, target_cont, train_indices, val_indices = load_data("cleveland")
-    construct_nn_reg(X_basic[train_indices, :], target_cont[train_indices],X_basic[val_indices], target_cont[val_indices], activation="relu", alpha = 0.0001, verbose = True, normalize = True)
-    construct_nn_class(X_basic[train_indices, :], target_bin[train_indices],X_basic[val_indices], target_bin[val_indices], activation="relu", alpha = 0.0001, verbose = True, normalize = True)
+    X_basic, X_technical, target_bin, target_cont, train_indices, val_indices = load_data("all")
+    #construct_nn_reg(X_basic[train_indices, :], target_cont[train_indices],X_basic[val_indices], target_cont[val_indices], activation="relu", alpha = 0.0001, verbose = True, normalize = True)
+    #construct_nn_class(X_basic[train_indices, :], target_bin[train_indices],X_basic[val_indices], target_bin[val_indices], activation="relu", alpha = 0.0001, verbose = True, normalize = True)
+    make_train_plot(X_basic, target_bin,train_indices, val_indices)
     # *** END CODE HERE ***
 
 if __name__ == '__main__':
--- nn_parameter_search.py
+++ nn_parameter_search.py
@@ -1,37 +1,49 @@
 from data_extraction import *
 from nn import *
-
+import matplotlib.pyplot as plt
 def run_nn_parameter_search(normalize, feature_type, type, filename):
     feature_list = ["basic", "technical"]
-    dataset_list = ["cleveland", "hungarian", "long_beach", "switzerland", "all"]
+    norm_list = [False, True]
+    #dataset_list = ["cleveland", "hungarian", "long_beach", "switzerland", "all"]
+    dataset_list = ["all"]
     activation_opt = ["identity", "relu", "logistic"]
     alpha_opt = [10,0.01,0.00001]
     reg_results = {}
     results_arr = np.empty((5,10), dtype = object)
+    train_accuracy_list = []
+    val_accuracy_list = []
     for inst_ind in range(len(dataset_list)):
         inst_name = dataset_list[inst_ind]
         X_basic, X_technical, target_bin, target_cont, train_indices, val_indices = load_data(inst_name)
         results_arr[inst_ind,0] = inst_name
-        if feature_type == "basic":
-            X = X_basic # use only basic features
-        else:
-            X = X_technical
-        for activation_ind in range(len(activation_opt)):
-            for alpha_ind in range(len(alpha_opt)):
-                if type == "reg":
-                    print(f"Fitting NN regression model on {inst_name} {feature_type} data with {activation_opt[activation_ind]} activation and degree {alpha_opt[alpha_ind]}.")
-                    lin_reg, pred_train, train_accuracy, train_mse, pred_val, val_mse, val_accuracy = \
-                    construct_nn_reg(X[train_indices, :], target_cont[train_indices],X[val_indices], target_cont[val_indices], \
-                    alpha=alpha_opt[alpha_ind], activation = activation_opt[activation_ind], verbose = True, normalize = normalize)
-                    results_arr[inst_ind, 1+3*activation_ind+alpha_ind] = str(val_accuracy)[0:4]
-                if type == "class":
-                    print(f"Fitting nn classification model on {inst_name} {feature_type} data with {activation_opt[activation_ind]} activation and degree {alpha_opt[alpha_ind]}.")
-                    log_reg, pred_train, train_accuracy, pred_val, val_accuracy = \
-                    construct_nn_class(X[train_indices, :], target_bin[train_indices],X[val_indices], target_bin[val_indices], \
-                    alpha=alpha_opt[alpha_ind], activation = activation_opt[activation_ind], verbose = True, normalize = normalize)
-                    results_arr[inst_ind, 1+3*activation_ind+alpha_ind] = str(val_accuracy)[0:4]
-
+        for feature_type in feature_list:
+            for normalize in norm_list:
+                if feature_type == "basic":
+                    X = X_basic # use only basic features
+                else:
+                    X = X_technical
+                for activation_ind in range(len(activation_opt)):
+                    for alpha_ind in range(len(alpha_opt)):
+                        if type == "reg":
+                            print(f"Fitting NN regression model on {inst_name} {feature_type} data with {activation_opt[activation_ind]} activation and degree {alpha_opt[alpha_ind]}.")
+                            lin_reg, pred_train, train_accuracy, train_mse, pred_val, val_mse, val_accuracy = \
+                            construct_nn_reg(X[train_indices, :], target_cont[train_indices],X[val_indices], target_cont[val_indices], \
+                            alpha=alpha_opt[alpha_ind], activation = activation_opt[activation_ind], verbose = True, normalize = normalize)
+                            results_arr[inst_ind, 1+3*activation_ind+alpha_ind] = str(val_accuracy)[0:4]
+                            train_accuracy_list.append(train_accuracy); val_accuracy_list.append(val_accuracy)
+                        if type == "class":
+                            print(f"Fitting nn classification model on {inst_name} {feature_type} data with {activation_opt[activation_ind]} activation and degree {alpha_opt[alpha_ind]}.")
+                            log_reg, pred_train, train_accuracy, pred_val, val_accuracy = \
+                            construct_nn_class(X[train_indices, :], target_bin[train_indices],X[val_indices], target_bin[val_indices], \
+                            alpha=alpha_opt[alpha_ind], activation = activation_opt[activation_ind], verbose = True, normalize = normalize)
+                            results_arr[inst_ind, 1+3*activation_ind+alpha_ind] = str(val_accuracy)[0:4]
+                            train_accuracy_list.append(train_accuracy); val_accuracy_list.append(val_accuracy)
     np.savetxt(filename, results_arr, delimiter=' & ', fmt = '%s', newline=' \\\\\n')
+    num_models = len(train_accuracy_list)
+    plt.clf()
+    plt.scatter(np.linspace(1,num_models, num_models, endpoint=True), np.array(train_accuracy_list), s = 40, alpha = 0.6, marker = "o", label = "train")
+    plt.scatter(np.linspace(1,num_models, num_models, endpoint=True), np.array(val_accuracy_list), s = 40, alpha = 0.6, marker = "d", label = "validation")
+    plt.xlabel("model trials"); plt.ylabel("accuray"); plt.legend(); plt.ylim([0.65, 1]); plt.savefig("train_val_plot", bbox_inches = "tight")
 # print("Unnormalized, Basic Features")
 # run_nn_parameter_search(False, feature_type = "basic", filename = "log_lin_val_table_basic.csv")
 print("Normalized, Basic Features")
